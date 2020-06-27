d601bb67d65399b0d0aeb9634cd4c23d2d866fe8
Lihang Yang <863141163@qq.com>
2020-06-27 11:41:32 +0800

    Remove element

---
+++ src/leetcode/remove-element/index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+const removeElement = function (nums, val) {
+    for (let i = 0, len = nums.length; i < len;) {
+        const num = nums[i];
+        if (num === val) {
+            for (let j = i + 1; j < len; j++) {
+                nums[j - 1] = nums[j];
+            }
+            nums.pop();
+            len = nums.length;
+        } else {
+            i++
+        }
+    }
+
+    return nums.length
+};
